c20e12a449a4d7bc46c6698063b80f7e140a6238
longuigon <longuigon+git@gmail.com>
2023-09-16 22:28:45 -0300

    Con excepciones unhandled, pero guardado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,6 +9,21 @@ namespace Program
         static void Main(string[] args)
         {
             Wizard PepeElMago = CharacterCreator.CreateWizard("Pepe", 500);
+            PepeElMago.AddSpellBook();
+            Wizard GonzaloElFlipanteHechizeroSublimeDeLosCojones = CharacterCreator.CreateWizard("Gonzalo", 500);
+            GonzaloElFlipanteHechizeroSublimeDeLosCojones.AddSpellBook();
+            Dwarf EnanoDeViral = CharacterCreator.CreateDwarf("Enanito", 300);
+            Elf PapaPitufo = CharacterCreator.CreateElf("Papi", 1500);
+            Item Bastoncito = ItemCreator.CreateItem("Bastón", 50, 10);
+            Item Espadita = ItemCreator.CreateItem("Espada", 100, 0);
+            Item Escudito = ItemCreator.CreateItem ("Escudo", 5, 50);
+            Spell Fueguito = Spell.CreateSpell("Fuego", 150, 0);
+            Spell Aguita = Spell.CreateSpell ("Agua", 20, 50);
+            Spell Tormentita = Spell.CreateSpell ("Tormenta", 75, 40);
+            PepeElMago.AddSpellToSpellBook(Fueguito);
+            GonzaloElFlipanteHechizeroSublimeDeLosCojones.AddSpellToSpellBook(Aguita);
+            PepeElMago.AttackWizard(GonzaloElFlipanteHechizeroSublimeDeLosCojones);
+            GonzaloElFlipanteHechizeroSublimeDeLosCojones.AttackWizard(PepeElMago);
         }
     }
 }
